88d6506ecce0f60bb05ee70b2b04af2554c00be2
banujan59 <banujan59@gmail.com>
2024-04-06 17:45:23 -0400

    Form adjustement & error check
    - Removed target audience (maybe a later feature?)
    - Error check for selected dates

--- app/form.js
+++ app/form.js
@@ -1,28 +1,47 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 
 export default function From()
 {
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [formOutput, setFormOutput] = useState('');
+
+    function ValidateDates(startDate, endDate) 
+    {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+      
+        if (end > start) 
+          return true;
+         else 
+          return false;
+      }
+
+    function SubmitForm(event)
+    {
+        event.preventDefault();
+        setFormOutput("");
+
+        if(startDate == "" || endDate == "" || !ValidateDates(startDate, endDate))
+            setFormOutput("Make sure the dates ranges are valid !");
+        
+    }
+
     return (
-        <form className='UserInputForm'>
+        <form className='UserInputForm' onSubmit={SubmitForm}>
             <h3>Choose date range of commits:</h3>
             <br/>
             <label>Start date:</label>
-            <input type="date" id="startDate" name="startDate"/>
+            <input type="date" id="startDate" name="startDate" value={startDate} onChange={(event) => { setStartDate(event.target.value);}}/>
 
             <label>End date:</label>
-            <input type="date" id="endDate" name="endDate"/>
-
-            <br/><br/>
-            
-            <h3>Target Audience:</h3>
-            <input type="radio" id="clientAudience" name="targetAudience" value="Clients"/>
-            <label for="clientAudience">Clients</label>
-            <input type="radio" id="developperAudience" name="targetAudience" value="Developper"/>
-            <label for="developperAudience">Developper</label>
+            <input type="date" id="endDate" name="endDate" value={endDate} onChange={(event) => { setEndDate(event.target.value);}}/>
 
             <br/><br/>
 
-            <button type='submit'>Submit</button>
+            <button>Submit</button>
+            <p>{formOutput}</p>
         </form>
     );
 }
