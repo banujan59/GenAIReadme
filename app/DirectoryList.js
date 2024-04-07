a35ffbef7f567be172d2957db0898279fe32bae3
banujan59 <banujan59@gmail.com>
2024-04-06 22:57:17 -0400

    Reorganize application and get rid of warnings

--- app/DirectoryList.js
+++ app/DirectoryList.js
@@ -1,20 +1,12 @@
+'use client'
 import React from 'react';
-import fs from 'fs';
-import githubProjectPath from "./GitHubProjectPaths"
 
-export default function DirectoryList()
+export default function DirectoryList({directories})
 {
-    const getDirectories = (source) => {
-        return fs.readdirSync(source, { withFileTypes: true })
-            .filter((dirent) => dirent.isDirectory())
-            .map((dirent) => dirent.name);
-    };
-    const directories = getDirectories(githubProjectPath);
-
     return (
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         {directories.map((dirName) => (
-            <span 
+            <span key={dirName}
             className="directoryBox group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             >
             <h2 className={`mb-3 text-2xl font-semibold`}>
