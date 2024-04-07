93a4644f14cae313110474b4d8f4143f2da0a4a5
banujan59 <banujan59@gmail.com>
2024-04-06 23:30:59 -0400

    Change project list presentation

--- app/DirectoryList.js
+++ app/DirectoryList.js
@@ -1,19 +1,26 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function DirectoryList({directories})
 {
+    const defaultIndex = 0;
+    const [selectedProject, setSelectedProject] = useState(directories[defaultIndex]);
+
+    function handleProjectChanged(event)
+    {
+        setSelectedProject(event.target.value);
+    }
+
     return (
-        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {directories.map((dirName) => (
-            <span key={dirName}
-            className="directoryBox group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-                {dirName}
-            </h2>
-            </span>
-        ))}
+        <div>
+            <p>Select the GitHub project:</p>
+            <br/>
+            {directories.map((dirName, index) => (
+                <span key={dirName} className='projectSpan'>
+                    <input type="radio" id={dirName} name="project" value={dirName} defaultChecked={index === defaultIndex} onChange={handleProjectChanged}/>
+                    <label htmlFor={dirName}> {dirName}</label>
+                </span>
+            ))}
       </div>
     );
 }
